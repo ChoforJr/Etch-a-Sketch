cacbed2a92e99cce5a479eda43b0ef69444a5755
ChoforJr <choforforsakang@gmail.com>
2024-06-20 22:42:42 +0100

    Added the hover Effect
    The hover effect was added completing the assignment

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 const button = document.querySelector("#button");
 const container = document.querySelector(".container");
+
 container.style.boxSizing = 'border-box';
 container.style.display = 'flex';
 container.style.flexDirection = 'row';
 container.style.flexWrap = 'wrap';
 container.style.justifyContent = 'space-between';
+
 let newDiv;
 
 function squaredGrid (gridNumbers){
   let gridSides = (480/gridNumbers);
   let gridNum = gridNumbers*gridNumbers;
   for(let i=0;i<gridNum;i++){
-    newDiv = document.createElement('div'); 
+    newDiv = document.createElement('div');
+    newDiv.style.boxSizing = 'border-box';
     newDiv.style.border = '1px solid black';
     newDiv.style.width = `${gridSides}px`;
     newDiv.style.height = `${gridSides}px`;
     container.appendChild(newDiv);
+    } 
+    container.addEventListener('mouseover', e => {
+      var target = e.target
+    
+      if (target !== container) {
+        target.classList.add('permahover');
+      }
+    })
   }
- 
-}
+
+
 squaredGrid(16);
-/*newDiv.addEventListener("mouseover",function(){
-  this.style.background = "black";
-});*/
+
 
 button.addEventListener("click", () => {
   let size = prompt("Input grid size");
